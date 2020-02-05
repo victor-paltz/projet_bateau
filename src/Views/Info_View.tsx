592d17e58822043f3bedc5320b35c318473aceb0
Zizol <adrien.videgrain@gmail.com>
2020-02-05 17:49:46 +0100

    Page info sommaire

--- src/Views/Info_View.tsx
+++ src/Views/Info_View.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import logoSopra from '../Assets/soprasteria_logo.png'
+import logoCS from '../Assets/LogoCS.png'
 
 export default class InfoView extends React.Component {
   constructor(props) {
@@ -8,9 +10,37 @@ export default class InfoView extends React.Component {
 
   render() {
     return (
-      <View>
-        <Text>Bonjour</Text>
+      <View style={[styles.container]}>
+        <Text style={[styles.text]}>Cette application a été crée par deux étudiants de CentraleSupélec,
+        dans le cadre de leur projet d'option en partenariat avec Sopra Steria</Text>
+        <Image
+          resizeMode="contain"
+          style={styles.image}
+          source={logoSopra} />
+        <Image
+          resizeMode="contain"
+          style={styles.image}
+          source={logoCS} />
       </View>
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  text: {
+    color: 'black',
+    fontSize: 20,
+    marginTop: 70,
+    marginHorizontal: 10,
+  },
+  image: {
+    marginTop: 50,
+    height: 100,
+    width: 100,
+  },
+});
