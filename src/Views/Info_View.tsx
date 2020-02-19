443a478c6c116812c3c1dfbead63ccef1d4b2bad
Victor Paltz <paltz.victor@free.fr>
2020-02-19 17:17:56 +0100

    fix android size

--- src/Views/Info_View.tsx
+++ src/Views/Info_View.tsx
@@ -33,16 +33,14 @@ const styles = StyleSheet.create({
   rect: {
     top: 0,
     left: 0,
-    width: 375,
-    height: 742,
     backgroundColor: "rgba(230, 230, 230,1)",
     position: "absolute"
   },
   materialCardWithoutImage1: {
-    width: 351,
     height: 443,
     marginTop: 34,
-    marginLeft: 12
+    marginLeft: 12,
+    marginRight: 12
   },
   materialIconTextButtonsFooter: {
     top: 739,
@@ -52,7 +50,5 @@ const styles = StyleSheet.create({
     left: 0
   },
   rectStack: {
-    width: 375,
-    height: 812
   }
 });
